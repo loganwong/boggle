b7104c090421b7b40cbef6d36cd0d8eb225edf32
Logan <seeking3226@gmail.com>
2017-11-04 15:20:29 -1000

    implemented binary word seach and added word list

---
+++ BinarySearch.py
@@ -0,0 +1,35 @@
+from arrayutils import arrayContent
+from time import time
+
+def find(array,word):
+    return rec_find(array, word, 0, len(array))
+def rec_find(array, word, start, end):
+    if end == start:
+        return False
+    mid = start + int((end - start)/2)
+    check = array[mid]
+    if check == word:
+        return True
+    elif check > word:
+        return rec_find(array, word, start, mid)
+    elif check < word:
+        return rec_find(array, word, mid + 1, end)
+
+def main():
+    with open('twl06.txt')as f:
+        lexicon = f.read().splitlines()
+
+    testwords = ['aardvark','drywall','offramp','zygote','ziop']
+    for testword in testwords:
+        start = time()
+        iscontained = arrayContent(lexicon,testword)
+        end = time()
+        print(testword,iscontained,end-start)
+    for testword in testwords:
+        start = time()
+        iscontained = find(lexicon,testword)
+        end = time()
+        print(testword,iscontained,end-start)
+
+
+main()
